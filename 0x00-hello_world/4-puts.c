8df6754f5e4377a7b836c82d4895c04054738ac5
yaw-caleb <g.0.d.d@outlook.com>
2022-12-07 22:57:34 +0000

    Add 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main (){
+
+	/*String Initialization*/
+	char MyStrg[] = "Programming is like building a multilingual puzzle";
+
+	puts(MyStrg);/*printing string to stdout*/
+
+	return 0;
+}
