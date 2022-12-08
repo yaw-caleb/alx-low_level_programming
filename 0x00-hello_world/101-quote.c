3107a3fbbb37d49a1188dbc829a678ef657ecfc3
yaw-caleb <g.0.d.d@outlook.com>
2022-12-08 21:52:46 +0000

    Add 101-htfx

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <unistd.h>
 /**
  * main - main fuction of the program
  * Description: A prgrom to print using the perror()
@@ -9,9 +9,8 @@
 int main(void)
 
 {
-	/* Using the perror function */
-	perror("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	/* Using the write functio */
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
-	return (-1);
-
+	return (1);
 }
