05a7a5d8378a64674865429f49fb29e13a66a945
yaw-caleb <g.0.d.d@outlook.com>
2022-12-08 00:38:28 +0000

    Add 5-file

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - main function of the file
+ * Description: A file to print out strings stored in the array declared
+ * Return: The file returns a value of zero to terminate the program
+ */
+
+int main(void)
+
+{
+	/* Declaration of strings using array */
+	char mystr[] = "with proper grammar, but the outcome is a piece of art,";
+
+	/* Printing string to stdout using the printf function */
+	printf("%s\n", mystr);
+
+	return (0);
+}
