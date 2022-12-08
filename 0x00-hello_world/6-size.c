c6ed8f6fa4db7d3d1324137c57004de43f92e223
yaw-caleb <g.0.d.d@outlook.com>
2022-12-08 18:27:28 +0000

    Htfx-6-2

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ int main(void)
 	/* main area of code execution */
 
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
-	printf("Size of an int: %lu bytes(s)\n", sizeof(int));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
 	printf("Size of a float: %lu byte(s)\n", sizeof(float));
-	printf("Size of a double: %lu bytes(s)\n", sizeof(double));
-	printf("Size of a long int: %lu bytes(s)\n", sizeof(long int));
+	printf("Size of a double: %lu byte(s)\n", sizeof(double));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
 	printf("Size of a 2x long int: %lu byte(s)\n", sizeof(long long int));
 	printf("Size of a long double: %lu byte(s)\n", sizeof(long double));
 
