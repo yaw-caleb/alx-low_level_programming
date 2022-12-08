c1c5eac49174f2202cafb48ad3ed008904675915
yaw-caleb <g.0.d.d@outlook.com>
2022-12-08 19:39:05 +0000

    add 101-file

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main fuction of the program
+ * Description: A prgrom to print using the perror()
+ * Return: returns a value of one
+ */
+
+int main(void)
+
+{
+	/* Using the perror function */
+	perror("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+
+	return (-1);
+
+}
