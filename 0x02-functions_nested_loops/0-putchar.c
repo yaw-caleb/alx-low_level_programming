dc6d917efcfa4cefa6d7ebe42fabd58bff634785
yaw-caleb <g.0.d.d@outlook.com>
2022-12-13 22:18:18 +0000

    fix1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - main function
+ *
+ * Description: print using putchar
+ *
+ * Return: return a value of zero
+ */
+
+int main(void)
+{
+	_putchar('\n');
+
+	return (0);
+
+}
