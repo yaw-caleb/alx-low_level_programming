815afbb6e8656975d3e5c06318fb39e097ee6d71
yaw-caleb <g.0.d.d@outlook.com>
2022-12-14 13:56:13 +0000

    Add-8-files

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints the minutes in a day
+ *
+ * Description: prints out every minutes of the date in  24-hr format
+ *
+ * Return: returns a print statement to stdout
+ */
+
+void jack_bauer(void)
+{
+	int x, y;
+
+	for (x = 0; x < 24; x++)
+	{
+		for (y = 0; y < 60; y++)
+		{
+			_putchar((x / 10) + '0');
+			_putchar((x % 10) + '0');
+			_putchar(':');
+			_putchar((y / 10) + '0');
+			_putchar((y % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
+
---
+++ 0x02-functions_nested_loops/8-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - check code
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	jack_bauer();
+	return (0);
+}
