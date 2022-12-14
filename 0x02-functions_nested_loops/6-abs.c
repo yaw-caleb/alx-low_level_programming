6e749ac4ef3551e947887d72b5962fa80e299dfc
yaw-caleb <g.0.d.d@outlook.com>
2022-12-14 12:27:11 +0000

    htfx-6-2

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -12,16 +12,16 @@ int _abs(int n)
 {
 	if (n > 0)
 	{
-		_putchar(n + '0');
+		return (n);
 	}
 	else if (n == 0)
 	{
-		_putchar('0');
+		return (0);
 	}
 	else if (n < 0)
 	{
 		n = n * -1;
-		_putchar(n + '0');
+		return (n);
 	}
 	return (0);
 }
