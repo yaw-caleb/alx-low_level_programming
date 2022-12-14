48920e33a5e4020b2cf357606b492a4283b34c59
yaw-caleb <g.0.d.d@outlook.com>
2022-12-14 14:29:15 +0000

    Add-10-files

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - Adds two integers
+ *
+ * Description: recieves two input to compute addition
+ *
+ * @n: a local vraiable
+ * @y: a local variable
+ *
+ * Return: returns result
+ */
+
+int add(int n, int y)
+{
+	return (n + y);
+}
