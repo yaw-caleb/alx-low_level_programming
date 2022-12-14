b1b4671e0c972331a60c2e4467d5daa8146301da
yaw-caleb <g.0.d.d@outlook.com>
2022-12-14 01:59:15 +0000

    Add 3-files

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _islower - function to validate lowercase
+ *
+ * @c: a local variable for the function
+ *
+ * Return: returns 1 if lowercase and 0 if otherwise
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
---
+++ 0x02-functions_nested_loops/3-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - check code.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int r;
+
+	r = _islower('H');
+	_putchar(r + '0');
+	_putchar('\n');
+
+	return (0);
+}
