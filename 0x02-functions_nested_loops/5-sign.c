c1cf6aaa3649ab8daa6d2199468f2443d7a75b5d
yaw-caleb <g.0.d.d@outlook.com>
2022-12-14 06:27:32 +0000

    Add-5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of a number
+ *
+ * Description: prints for both positive,negative and zero
+ *
+ * @n: a local variable in the function
+ * Return: Returns a value of 1, 0 and -1
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+
+		return (-1);
+	}
+	
+	return (0);
+
+}
