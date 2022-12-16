b0f27f7df97981e60eb2bc38c35f6ec4c77a0812
yaw-caleb <g.0.d.d@outlook.com>
2022-12-16 23:40:45 +0000

    add-1

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("infinite loop incoming :(\n");
+
+	i = 0;
+	/**
+
+	while (i < 10)
+	{
+		putchar(i);
+	}
+	*/
+
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}
+		
+
