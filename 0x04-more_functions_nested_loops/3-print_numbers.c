afddb37e88bc2322a981e064b4cd863444f0cebb
yaw-caleb <g.0.d.d@outlook.com>
2022-12-16 20:56:59 +0000

    Add-3-files

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints from 0 to 9
+ *
+ * Description: Doesnt print 2 and 4
+ *
+ * Return: returns void
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i + '0');
+	}
+
+	_putchar('\n');
+
+
