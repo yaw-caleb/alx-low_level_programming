4adb1ccbdaec2f73bf9e375ef722df14126c80ca
yaw-caleb <g.0.d.d@outlook.com>
2022-12-20 05:08:52 +0000

    Add-10-file

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,4 +10,35 @@
 
 int _atoi(char *s)
 {
+	int c = 0;
+	unsigned int n  = 0;
+	int m = 1;
+	int j = 0;
+
+	while (s[c])
+	{
+		if (s[c] == 45)
+		{
+			m *= -1;
+		}
+
+		while (s[c] >= 48 && s[c] <= 57)
+		{
+			j = 1;
+			n = (n * 10) + (s[c] - '0');
+			c++;
+		}
+
+		if (j == 1)
+		{
+			break;
+		}
+
+		c++;
+	}
+	n *= m;
+
+	return (n);
+}
+
 
