f4d80caf684d7cc9d70fd0e6f3cedbb4eb7f06c0
yaw-caleb <g.0.d.d@outlook.com>
2023-01-05 22:31:30 +0000

    Add-1-files

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - fxn that prints a string in reverse
+ *
+ * @s: a local pointer var
+ *
+ * Return: returns void
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+	}
+
+	else
+	{
+		return;
+	}
+
+	_putchar(*s);
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -4,5 +4,6 @@
 int _putchar(char c);
 #include <stdio.h>
 void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
 
 #endif/*MAIN_H*/
