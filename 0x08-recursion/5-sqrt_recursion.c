63efc33935af1f1270825675e844eab6301be84e
yaw-caleb <g.0.d.d@outlook.com>
2023-01-05 23:29:26 +0000

    fix-5-1

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -10,15 +10,7 @@
 
 int _sqrt_recursion(int n)
 {
-	if (n < 0)
-	{
-		return (-1);
-	}
-
-	else
-	{
-		return (_sqrt(n, (n + 1) / 2));
-	}
+	return (_sqrt(n, 1));
 }
 
 /**
@@ -33,13 +25,13 @@ int _sqrt_recursion(int n)
 
 int _sqrt(int n, int i)
 {
+	int s = i * i;
 
-	if (i < 1)
+	if (s > n )
 		return (-1);
 
-	else if (i * i == n)
+	if (s == n)
 		return (i);
 
-	else
-		return (_sqrt(n, i - 1));
+	return (_sqrt(n, i + 1));
 }
