dc1d85320b2e6fdfde7f55fd07472e63e6762c4a
yaw-caleb <g.0.d.d@outlook.com>
2023-01-16 14:19:47 -0800

    Add-2-files

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - wrtite a fxn that prints a striuct dog
+ * @d: struct name
+ * Return:0
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+	if (d->name == NULL)
+		d->name = "(nil)";
+	if (d->age <= 0)
+		printf("Age: (nil)\n");
+	if (d->owner == NULL)
+		d->owner = "(nil)";
+
+	printf("Name: %s\n", d->name);
+	printf("Age: %.6f\n", d->age);
+	printf("Owner: %s\n", d->owner);
+
+}
+
